895732fa635d0f28a7f6309f794f761b24df4df5
Ana Valdivia <anav1317@yahoo.com>
2020-07-08 21:31:20 -0700

    Added 21-23

---
+++ 21-24_HH_BasicsA.js
@@ -0,0 +1,50 @@
+// #21
+// Objectives
+// Given two numbers passed to the function, return a new array of length num1 with each value num2. For example, thisLengthThatValue(3,5) should return [5, 5, 5]. thisLengthThatValue(2, 10) should return [10, 10].
+
+function thisLengthThatValue(num1, num2){
+    var arr = []
+
+    for (var i=0; i < num1; i++){
+        arr.splice(i,0,num2)
+    }
+    return arr 
+}
+
+// #22
+// Objectives
+// Build function that accepts array. Return a new array with all values except first, adding 7 to each. Do not alter the original array.
+
+// For example, addSevenToMost([1,3,5]) should return [10, 12]. addSevenToMost([1, 3, 5, 7]) should return [10, 12, 14].
+
+function addSevenToMost(arr){
+    var newArr = []
+
+    for (var i=1; i<arr.length; i++){
+        var timesSeven = arr[i]+7
+        newArr.push(timesSeven)
+    }
+    return newArr 
+}
+
+// #23
+// // Objectives
+// Write a function that accepts any array, and returns a new array with the array values that are greater than its 2nd value. If the array that was passed to the function has less than two elements, have the function return false.
+
+// For example greaterThanSecond([1,3,5,7]) should return [5, 7]. Similarly, greaterThanSecond([0, -3, 2, 5]) should return [0, 2, 5]. Also, greaterThanSecond([2]) should return false.
+
+function greaterThanSecond(arr){
+    var newArr = []
+
+    if (arr.length <= 1){
+        return false
+    }
+    else {
+        for (var i=0; i<arr.length; i++){
+            if (arr[i] > arr[1]){
+            newArr.push(arr[i])     
+            }       
+        }
+    return newArr   
+    }
+}
